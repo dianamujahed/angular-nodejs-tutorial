2b36441ca56140034c690dab5d6795ac9be373b4
Diana Mujahed <dmujahed@asaltech.com>
2023-09-21 11:52:03 +0300

    login  page

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -11,13 +12,18 @@ import { Food } from 'src/app/shared/models/Food';
 export class HomeComponent {
   foods:Food[] = [];
   constructor(private foodService: FoodService, activatedRoute:ActivatedRoute) {
+    let foodObservable : Observable<Food[]>;
     activatedRoute.params.subscribe((params) => {
       if(params["searchTerm"])
-        this.foods = this.foodService.getAllFoodsBySearchTerm(params["searchTerm"]);
+        foodObservable = this.foodService.getAllFoodsBySearchTerm(params["searchTerm"]);
       else if(params["tag"])
-        this.foods = this.foodService.getAllFoodByTag(params["tag"]);
+        foodObservable = this.foodService.getAllFoodsByTag(params["tag"]);
       else  
-        this.foods = foodService.getAll();
+        foodObservable = foodService.getAll();
+
+        foodObservable.subscribe((serverFoods) => {
+          this.foods = serverFoods;
+        })
     })
   }
 }
