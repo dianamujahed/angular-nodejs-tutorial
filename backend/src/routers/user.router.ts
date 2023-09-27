a28e7668d977c2730740905cfccba12f0a8efd2a
Diana Mujahed <dmujahed@asaltech.com>
2023-09-27 14:14:01 +0300

    register page

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -3,6 +3,8 @@ import { sample_users } from '../data';
 import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
 import { User, UserModel } from '../models/user.model';
+import { HTTP_BAD_REQUEST } from '../constants/http_status';
+import bcrypt from 'bcryptjs';
 
 const router = Router();
 
@@ -22,20 +24,53 @@ router.get("/seed", asyncHandler(
 router.post("/login", asyncHandler(
   async (req, res) => {
     const {email, password} = req.body;
-    const user = await UserModel.findOne({email , password});
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    console.log(encryptedPassword)
 
-     if(user) {
+    const user = await UserModel.findOne({email});
+    let correctPass = false;
+    if(user) {
+      correctPass = await bcrypt.compare(password, user.password);
+    }
+
+    if(user && correctPass) {
       res.send(generateTokenReponse(user));
-     }
-     else{
-       const BAD_REQUEST = 400;
-       res.status(BAD_REQUEST).send("Username or password is invalid!");
-     }
+    } else {
+        res.status(HTTP_BAD_REQUEST).send("Username or password is invalid!");
+    }
 
   }
 ))
 
+router.post('/register', asyncHandler(
+    async (req, res) => {
+      const {name, email, password, address} = req.body;
+      const user = await UserModel.findOne({email});
+      if(user){
+        res.status(HTTP_BAD_REQUEST)
+        .send('User is already exist, please login!');
+        return;
+      }
+
+      const encryptedPassword = await bcrypt.hash(password, 10);
+  
+      const newUser = {
+        name,
+        email: email.toLowerCase(),
+        password: encryptedPassword,
+        address,
+        isAdmin: false
+      }
+  
+      const dbUser = await UserModel.create(newUser);
+
+      res.send(generateTokenReponse(dbUser));
+    }
+  ))
+  
+
   const generateTokenReponse = (user : User) => {
+
     const token = jwt.sign({
       email:user.email, isAdmin: user.isAdmin
     },process.env.JWT_SECRET!,{
